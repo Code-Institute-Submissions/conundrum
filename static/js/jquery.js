35aa737f64aa96e59cb20f09b027c38b1318c27f
Brett Cutting <brettcutt@outlook.com.au>
2018-09-19 14:49:27 +0000

    added jquery to rules button in conundrum.html

---
+++ static/js/jquery.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".rules-btn").click(function() {
+        $("#game-rules").toggleClass("game-rules-hidden");
+        $("#game-rules").toggleClass("game-rules-show");
+    });
+});
