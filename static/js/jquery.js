bc9fa6ee1ba78b0fbbde917fddaa6241d7727760
Brett Cutting <brettcutt@outlook.com.au>
2018-09-24 04:50:42 +0000

    fixed footer problem, added welcome message to conundrum and and different message if user gets on the leaderboard

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
     $(".rules-btn").click(function() {
-        $("#game-rules").toggleClass("game-rules-hidden");
-        $("#game-rules").toggleClass("game-rules-show");
+        $("#game-rules").toggle('fade', 1000);
+    });
+    
+    $(".game-rules-ok-btn").click(function() {
+        $("#game-rules").hide('fade', 1000);
+    });
+    
+    
+    $("#welcome-screen-button").click(function() {
+        $("#welcome-screen").hide('fade', 1000);
     });
 });
