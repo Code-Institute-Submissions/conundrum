d537f4d69efb0f1ba66a49b2c12360e5cd1bb6fd
Brett Cutting <brettcutt@outlook.com.au>
2018-09-25 02:10:07 +0000

    changed positve/ nagative score position and so that is fades out over time

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -7,8 +7,9 @@ $(document).ready(function() {
         $("#game-rules").hide('fade', 1000);
     });
     
+    $('#welcome-screen').delay(2000).fadeOut(1500);
     
-    $("#welcome-screen-button").click(function() {
-        $("#welcome-screen").hide('fade', 1000);
-    });
+    $('.display_positive_negative_points').delay(3000).fadeOut(1500);
+    
 });
+
